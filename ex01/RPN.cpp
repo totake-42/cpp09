47e97fc65446b53ed148eb9a65c675a845050187
Taishi Otake <totake@student.42tokyo.jp>
2026-09-23 12:34:57 +0900

    fix: I changed ex01 container std::stack -> std::stak<int, std::list<int> >

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -1,78 +1,68 @@
 #include "RPN.hpp"
 #include <cctype>
+#include <list>
+#include <sstream>
 #include <stack>
 #include <stdexcept>
-#include <sstream>
 
-RPN::RPN()
-{
+RPN::RPN() {}
+
+RPN::RPN(const RPN &other) { (void)other; }
+
+RPN::~RPN() {}
+
+RPN &RPN::operator=(const RPN &other) {
+  (void)other;
+  return (*this);
 }
 
-RPN::RPN(const RPN &other)
-{
-	(void)other;
-}
+int RPN::calculate(const std::string &expression) const {
+  std::stack<int, std::list<int> > values;
+  std::istringstream stream(expression);
+  std::string token;
 
-RPN::~RPN()
-{
-}
+  while (stream >> token) {
+    if (token.length() != 1)
+      throw std::runtime_error("Invalid token in expression");
 
-RPN &RPN::operator=(const RPN &other)
-{
-	(void)other;
-	return (*this);
-}
+    char value = token[0];
 
-int RPN::calculate(const std::string &expression) const
-{
-	std::stack<int> values;
-	std::istringstream stream(expression);
-	std::string token;
+    if (std::isdigit(static_cast<unsigned char>(value))) {
+      values.push(value - '0');
+      continue;
+    }
 
-	while (stream >> token)
-	{
-		if (token.length() != 1)
-			throw std::runtime_error("Invalid token in expression");
+    if (value != '+' && value != '-' && value != '*' && value != '/')
+      throw std::runtime_error("Invalid token in expression");
 
-		char value = token[0];
+    if (values.size() < 2)
+      throw std::runtime_error("Insufficient values in expression");
 
-		if (std::isdigit(static_cast<unsigned char>(value)))
-		{
-			values.push(value - '0');
-			continue;
-		}
+    int right = values.top();
+    values.pop();
 
-		if (value != '+' && value != '-' && value != '*' && value != '/')
-			throw std::runtime_error("Invalid token in expression");
+    int left = values.top();
+    values.pop();
 
-		if (values.size() < 2)
-			throw std::runtime_error("Insufficient values in expression");
+    if (value == '/' && right == 0)
+      throw std::runtime_error("Division by zero");
 
-		int right = values.top();
-		values.pop();
+    int result = 0;
 
-		int left = values.top();
-		values.pop();
+    if (value == '+')
+      result = left + right;
+    else if (value == '-')
+      result = left - right;
+    else if (value == '*')
+      result = left * right;
+    else if (value == '/')
+      result = left / right;
 
-		if (value == '/' && right == 0)
-			throw std::runtime_error("Division by zero");
+    values.push(result);
+  }
 
-		int result = 0;
+  if (values.size() != 1)
+    throw std::runtime_error("Invalid expression");
 
-		if (value == '+')
-			result = left + right;
-		else if (value == '-')
-			result = left - right;
-		else if (value == '*')
-			result = left * right;
-		else if (value == '/')
-			result = left / right;
-
-		values.push(result);
-	}
-
-	if (values.size() != 1)
-		throw std::runtime_error("Invalid expression");
-
-	return values.top();
+  return values.top();
 }
